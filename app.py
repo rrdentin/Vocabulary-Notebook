70a6dae7abd9d5608b1740defa3e59b51947d033
rrdentin <denti.dona@gmail.com>
2023-07-10 20:33:52 +0700

    update

--- app.py
+++ app.py
@@ -15,14 +15,17 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
+
 app = Flask(__name__)
-
-password = 'sparta'
-cxn_str = f'mongodb+srv://test:{password}@cluster0.d5baau6.mongodb.net/'
-client = MongoClient(cxn_str)
-
-db = client.dbsparta_plus_week2
-
 
 @app.route('/')
 def main():
